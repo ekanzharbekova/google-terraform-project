31b2cd3409fd5edf14ecd665fa6fc8d4d483cbd6
ekanzharbekova <kanjarbekovaeli@gmail.com>
2022-11-30 01:50:47 +0000

    added account info

---
+++ account_setup/provider.tf
@@ -0,0 +1,18 @@
+data "google_billing_account" "acct" {
+	display_name = "My Billing Account 1"
+	open = true
+}
+
+resource "random_password" "password" {
+	length = 16
+	numeric = false
+	special = false
+	lower = true
+	upper = false
+}
+
+resource "google_project" "terraform-project" {
+	name = "terraform-project"
+	project_id = random_password.password.result
+	billing_account = data.google_billing_account.acct.id
+}
